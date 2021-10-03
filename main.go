42944002711229cef11781c4f3af31707580d032
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-10-03 10:04:50 +0900

    追加

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"html/template"
+	"net/http"
+	"log"
 	"github.com/astaxie/session"
 	_ "github.com/astaxie/session/providers/memory"
 )
@@ -12,6 +15,23 @@ func init() {
 	go globalSessions.GC()
 }
 
+func count(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	ct := sess.Get("countnum")
+	if ct == nil {
+		sess.Set("countnum", 1)
+	} else {
+		sess.Set("countnum", (ct.(int) + 1))
+	}
+	t, _ := template.ParseFiles("public/count.html")
+	w.Header().Set("Content-Type", "text/html")
+	t.Execute(w, sess.Get("countnum"))
+}
+
 func main() {
-	
+	http.HandleFunc("/count", count)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
